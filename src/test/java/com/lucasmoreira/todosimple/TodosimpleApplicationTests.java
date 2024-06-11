8ad27a9ddb06247a3df7ac97e4c488ae1a4a397e
Lucas Moreira <lucasmoreiralana04@gmail.com>
2024-06-11 14:15:34 -0300

    Spring Boot project

---
+++ src/test/java/com/lucasmoreira/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoreira.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
